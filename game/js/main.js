2419ca4e3576a6cd51bcf8d890b06742dabe04ba
HeliotropeTheory <26067464+HeliotropeTheory@users.noreply.github.com>
2020-02-22 23:17:26 -0800

    Update main.js

--- game/js/main.js
+++ game/js/main.js
@@ -1,12 +1,39 @@
 //var game;
 //var controls;
 window.onload = function(){
-		var config = {
-				type: Phaser.AUTO,
-				width: 864,
-				height: 608,
-				parent: 'phaser-game',
-				//scene: [SceneMain]
-		};
-		var game = new Phaser.Game(config);
+			var config = {
+	type: Phaser.AUTO,
+	scale: {
+		//mode: Phaser.Scale.FIT,
+		width: 800,
+		height: 600,
+		//min: {
+			//width: 800,
+			//height: 600,
+		//}
+	},
+	
+	physics: {
+		default: 'arcade',
+		arcade: {
+			debug: false,
+		}
+	},
+		
+		//Parent set the DOM id of the game 
+		//which allows the html file to referrence it.
+		parent: 'phaser-game',
+		scene: [ 
+			SceneMainMenu,
+			SceneMain,
+			SceneGameOver
+		]
+		
+		/*{
+			preload: preload,
+			create: create,
+			update: update
+		}*/
+	};
+	var game = new Phaser.Game(config);
 }
