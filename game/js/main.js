94e2ad06caeca055dbe50ee7ff0311dd2173915b
HeliotropeTheory <26067464+HeliotropeTheory@users.noreply.github.com>
2020-02-22 23:24:09 -0800

    Update main.js

--- game/js/main.js
+++ game/js/main.js
@@ -1,24 +1,17 @@
 //var game;
 //var controls;
 window.onload = function(){
-			var config = {
-	type: Phaser.AUTO,
-	scale: {
-		//mode: Phaser.Scale.FIT,
+	var config = {
+		type: Phaser.AUTO,
 		width: 800,
 		height: 600,
-		//min: {
-			//width: 800,
-			//height: 600,
-		//}
-	},
 	
-	physics: {
-		default: 'arcade',
-		arcade: {
-			debug: false,
-		}
-	},
+		physics: {
+			default: 'arcade',
+			arcade: {
+				debug: false,
+			}
+		},
 		
 		//Parent set the DOM id of the game 
 		//which allows the html file to referrence it.
